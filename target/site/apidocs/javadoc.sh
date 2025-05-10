c16f70e4d4582215826ee6aa41b76519058e17ac
xiaoqunLiu <12110943@mail.sustech.edu.cn>
2025-05-10 20:23:35 +0800

    jacoco

--- target/site/apidocs/javadoc.sh
+++ target/site/apidocs/javadoc.sh
@@ -1,1 +1,1 @@
-/opt/homebrew/Cellar/openjdk/23.0.2/libexec/openjdk.jdk/Contents/Home/bin/javadoc @options @packages
+/opt/homebrew/Cellar/openjdk@21/21.0.7/libexec/openjdk.jdk/Contents/Home/bin/javadoc @options @packages
